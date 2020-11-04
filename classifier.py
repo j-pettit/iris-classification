fc47c29bdfd42f656ed01316039b5ac709dfc065
Julian Pettit <julian.s.pettit@gmail.com>
2020-11-04 16:09:25 -0500

    make predictions with lda

--- classifier.py
+++ classifier.py
@@ -8,6 +8,7 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 # load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
@@ -20,12 +21,23 @@ X = arr[:,0:4]
 y = arr[:,4]
 X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.2, random_state=1)
 
-# make predictions on validation dataset
-model = SVC(gamma='auto')
-model.fit(X_train, y_train)
-predictions = model.predict(X_validation)
+# make predictions on validation dataset (svc)
+model_svc = SVC(gamma='auto')
+model_svc.fit(X_train, y_train)
+predictions_svc = model_svc.predict(X_validation)
 
-# Evaluate predictions
-print(accuracy_score(y_validation, predictions))
-print(confusion_matrix(y_validation, predictions))
-print(classification_report(y_validation, predictions))
+# make predictions on validation dataset (lda)
+# unverified
+model_lda = LinearDiscriminantAnalysis()
+model_lda.fit(X_train, y_train)
+predictions_lda = model_lda.predict(X_validation)
+
+# evaluate predictions
+print('SVC----------')
+print(accuracy_score(y_validation, predictions_svc))
+print(confusion_matrix(y_validation, predictions_svc))
+print(classification_report(y_validation, predictions_svc))
+print('LDA----------')
+print(accuracy_score(y_validation, predictions_lda))
+print(confusion_matrix(y_validation, predictions_lda))
+print(classification_report(y_validation, predictions_lda))
